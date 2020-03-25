58256bfad204b9be1b381b8d9487105d9fd3b247
hxkang <hxkang@thoughtworks.com>
2020-03-25 20:55:47 +0800

    Make the task 1 pass the unit test

--- src/main/java/cn/xpbootcamp/gilded_rose/Locker.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Locker.java
@@ -2,12 +2,15 @@ package cn.xpbootcamp.gilded_rose;
 
 public class Locker {
     private int capacity;
+    private int availableBoxCount;
 
     public Locker(int capacity) {
         this.capacity = capacity;
+        availableBoxCount = capacity;
     }
 
-    public static Ticket save() {
+    public Ticket save() {
+        availableBoxCount--;
         return new Ticket();
     }
 
@@ -16,6 +19,6 @@ public class Locker {
     }
 
     public int availableBoxSize() {
-        return getCapacity();
+        return availableBoxCount;
     }
 }
--- src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
@@ -10,7 +10,7 @@ public class LockerTest {
     void should_return_ticket_successfully_when_save_a_package_in_1_locker_given_there_is_19_empty_box() {
         int capacity = 19;
         Locker locker = new Locker(capacity);
-        Ticket ticket = Locker.save();
+        Ticket ticket = locker.save();
         assertNotNull(ticket);
         assertEquals(capacity-1, locker.availableBoxSize());
     }
