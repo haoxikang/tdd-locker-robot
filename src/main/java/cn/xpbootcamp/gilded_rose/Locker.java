96e471468e51d5af1d16af68168ef20271238d0f
hxkang <hxkang@thoughtworks.com>
2020-03-25 20:47:39 +0800

    Fix compile errors for unit test of task 1

---
+++ src/main/java/cn/xpbootcamp/gilded_rose/Locker.java
@@ -0,0 +1,21 @@
+package cn.xpbootcamp.gilded_rose;
+
+public class Locker {
+    private int capacity;
+
+    public Locker(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public static Ticket save() {
+        return new Ticket();
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int boxSize() {
+        return getCapacity();
+    }
+}
--- src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
@@ -9,7 +9,7 @@ public class LockerTest {
     @Test
     void should_return_ticket_successfully_when_save_a_package_in_1_locker_given_there_is_19_empty_box() {
         Locker locker = new Locker(19);
-        Tickit tickit = Locker.save();
+        Ticket ticket = Locker.save();
         assertNotNull(ticket);
         assertEquals(18, locker.boxSize());
     }
