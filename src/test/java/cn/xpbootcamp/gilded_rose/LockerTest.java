24920a06c9a347ee6430559244338a211afb8188
wan-si <swan@thoughtworks.com>
2020-03-26 20:34:38 +0800

    code test of task 8

--- src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
@@ -77,6 +77,18 @@ public class LockerTest {
         Ticket ticket = locker.save();
         assertEquals(capacity - 1, locker.availableBoxSize());
     }
+
+    @Test
+    void should_left_19_empty_boxes_when_take_a_package_given_there_are_18_box_available(){
+        int capacity = 18;
+        Locker locker = new Locker(capacity);
+
+        Ticket ticket = new Ticket("12345678","2");
+        String result = locker.takePackage(ticket);
+
+        assertEquals("打开" + ticket.getBoxNumber() + "号箱子", result);
+        assertEquals(capacity + 1, locker.availableBoxSize());
+    }
     
 
 }
